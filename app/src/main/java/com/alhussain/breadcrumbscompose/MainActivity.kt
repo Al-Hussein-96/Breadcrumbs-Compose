6ddb2b3b2f5f1ae3890c5a7eabcec88b9c5628e2
alhussain <mohammad.alhusain@shopiniworld.com>
2023-06-28 02:39:47 +0400

    Initial commit

---
+++ app/src/main/java/com/alhussain/breadcrumbscompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alhussain.breadcrumbscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alhussain.breadcrumbscompose.ui.theme.BreadcrumbsComposeTheme
+import com.alhussain.library.UnlimitedBreadCrumbs
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BreadcrumbsComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnlimitedBreadCrumbs()
+
+                }
+            }
+        }
+
+        val x = 5;
+
+        with(x){
+            
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BreadcrumbsComposeTheme {
+        UnlimitedBreadCrumbs()
+    }
+}
---
+++ library/src/main/java/com/alhussain/library/UnlimitedBreadCrumbs.kt
@@ -0,0 +1,9 @@
+package com.alhussain.library
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UnlimitedBreadCrumbs() {
+    Text("This is my custom view")
+}
